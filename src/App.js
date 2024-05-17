83c3366b6a7381118e1d1f79eea002b438d6a32c
erdemkaya34 <145977483+erdemkaya34@users.noreply.github.com>
2024-05-17 16:43:56 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css"
 
 function App() {
   return (
-    <div className='bg-green-400 w-1/3 flex h-full'>
+    <div className='bg-green-400 w-2/5  flex h-full'>
     <div className='h-full flex justify-center items-center'>
-          <div className="mt-20 ml-28">
-            <div className=' p-24  flex border border-gray-300  shadow-2xl' >
+          <div className="mt-9 ml-28">
+            <div className=' p-24  flex border border-gray-300 mb-36  shadow-2xl' >
               <div className='mr-66 mt-14 '><img src={logo} alt='asda' className='w-72 mr-96'  /></div>
-              <div className='mt-20 '>
-                <h1 className='text-center mr-12 text-3xl' >LOGIN</h1>
-                <input type="text" placeholder='Username' class=" mt-5 border border-gray-300  shadow-lg px-8 py-2 focus:outline-none focus:border-blue-500 w-80"/><br></br>
-                <input type="password" placeholder='Password' class= " mt-5 border border-gray-300  shadow-sm px-8 py-2 focus:outline-none focus:border-blue-500 w-80"/>
-                <div>
-                <button className=' bg-green-300 text-white mt-5 border border-gray-300  shadow-2xl px-4 py-2 focus:outline-none focus:border-blue-500 w-40'>LOGIN</button>
-                <a href='^^#' className='mr-10 ml-12 text-xs ' >Forgot password  register</a>
+              <div className='mt-20 mr-30   '>
+                <div className="mr-12 mb-16 ">
+                <h1 className='text-center ml-28 text-6xl font-bold text-gray-700' >LOGIN</h1>
+                <input type="text" placeholder='Username' class=" mt-5 border ml-32 border-gray-300  shadow-lg px-8 py-2 focus:outline-none focus:border-blue-500 w-80"/><br></br>
+                <input type="password" placeholder='Password' class= " mt-5 border ml-32 border-gray-300  shadow-sm px-8 py-2 focus:outline-none focus:border-blue-500 w-80"/>
+                <div className="flex mr-6 gap-8" >
+                <button className=' bg-green-300 ml-32 text-white mt-5 border border-gray-300  shadow-2xl px-4 py-2 focus:outline-none focus:border-blue-500 w-40'>LOGIN</button>
+                <a href='#' className='block mt-6 flex justify-end ml-20 text-xs'>Forgot password register</a>
+                </div>  
               </div>              
               </div>
             </div>
@@ -24,5 +26,3 @@ function App() {
     </div>
   )
 }
-
-export default App
